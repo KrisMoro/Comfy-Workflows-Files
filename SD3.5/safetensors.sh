9ac815d3a77589ed270d3f7ac430ffd86b7d8feb
Sergey Morotsky <morotsky@gmail.com>
2025-01-13 18:41:05 +0100

    Update safetensors.sh

--- SD3.5/safetensors.sh
+++ SD3.5/safetensors.sh
@@ -6,7 +6,7 @@
 
 # Packages are installed after nodes so we can fix them...
 
-DEFAULT_WORKFLOW="https://raw.githubusercontent.com/ai-dock/comfyui/main/config/workflows/flux-comfyui-example.json"
+DEFAULT_WORKFLOW="https://raw.githubusercontent.com/KrisMoro/Comfy-Workflows-Files/refs/heads/main/SD3.5/workflow-sd3.5-safetensors.json"
 
 APT_PACKAGES=(
     #"package-1"
